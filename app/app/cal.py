de5e207019a7006b8b3a24e9c957f4cec792e9e9
harikunapuli <kunapuliharishankar@gmail.com>
2025-11-28 09:30:52 +0530

    Added db and it's config

--- app/app/cal.py
+++ app/app/cal.py
@@ -1,11 +1,13 @@
 """
-calculater functions
+calculator functions
 
 """
+
 
 def add(x, y):
     """Add x and y"""
     return x + y
+
 
 def subtract(x, y):
     """Subtract x and y"""
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,18 +1,20 @@
 """
 sample test
 """
+
 from django.test import SimpleTestCase
 from app import cal
+
 
 class calcTests(SimpleTestCase):
     """test calc module"""
 
     def test_add_numbers(self):
         """test adding numbers together"""
-        res = cal.add(5,6)
+        res = cal.add(5, 6)
         self.assertEqual(res, 11)
+
     def test_subtract_numbers(self):
         """test subtracting numbers"""
-        res = cal.subtract(15,10)
+        res = cal.subtract(15, 10)
         self.assertEqual(res, 5)
-
